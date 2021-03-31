f9d7a9a3a821adeb73162120036b7499a95a27e1
Tsekko <maxime.kervadec@laposte.net>
2021-03-31 14:25:48 +0200

    Finalisation de la classe ScoreViewModel

--- app/src/main/java/com/example/clickncoop/ScoreViewModel.java
+++ app/src/main/java/com/example/clickncoop/ScoreViewModel.java
@@ -1,4 +1,27 @@
 package com.example.clickncoop;
 
-public class ScoreViewModel {
+import android.app.Application;
+
+import java.util.List;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+public class ScoreViewModel extends AndroidViewModel {
+    private ScoreRepository mRepository;
+    private LiveData<List<Score>> mAllScore;
+
+    public ScoreViewModel(Application application){
+        super(application);
+        mRepository = new ScoreRepository(application);
+        mAllScore = mRepository.getAllScore();
+    }
+
+    public LiveData<List<Score>> getAllScore(){
+        return mAllScore;
+    }
+
+    public void insert(Score score){
+        mRepository.insert(score);
+    }
 }
