ae54dde538ba0727905982dbd23c6554345e554e
kenan <kenan@DESKTOP-VN48SV7>
2021-04-01 23:00:02 +0200

    compteur + compte à rebours ajouté => finir de régler le compte à rebours

--- app/src/main/java/com/example/clickncoop/SecondFragment.java
+++ app/src/main/java/com/example/clickncoop/SecondFragment.java
@@ -1,16 +1,27 @@
 package com.example.clickncoop;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-public class SecondFragment extends Fragment {
+import com.example.clickncoop.CountViewModel;
+import com.example.clickncoop.R;
 
+import java.util.Locale;
+
+public class SecondFragment extends Fragment {
+    private int count = 0;
+    private int count2 = 0;
+    private CountViewModel model;
+    private CountViewModel model2;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -23,12 +34,54 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        TextView textView = (TextView) view.findViewById(R.id.text_view);
+
+        CountDownTimer countDownTimer = new CountDownTimer(10000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                textView.setText(String.format(Locale.getDefault(), "%d sec", millisUntilFinished / 1000L));
+            }
+
+            public void onFinish() {
                 NavHostFragment.findNavController(SecondFragment.this)
                         .navigate(R.id.action_SecondFragment_to_FirstFragment);
             }
-        });
+        }.start();
+
+        view.findViewById(R.id.button_second).setOnClickListener(view1 -> NavHostFragment.findNavController(SecondFragment.this)
+                .navigate(R.id.action_SecondFragment_to_FirstFragment));
+
+        view.findViewById(R.id.button_joueur2).setOnClickListener(view2 -> plusCount2());
+        view.findViewById(R.id.button_joueur1).setOnClickListener(view1 -> plusCount());
+
+        model = new ViewModelProvider(requireActivity()).get(CountViewModel.class);
+        count = model.getCompte();
+        modifyTextView(count);
+
+        model2 = new ViewModelProvider(requireActivity()).get(CountViewModel.class);
+        count2 = model2.getCompte();
+        modifyTextView2(count2);
+    }
+
+
+    private void plusCount(){
+        count++;
+        model.setCompte(count);
+        modifyTextView(count);
+    }
+
+    private void plusCount2(){
+        count2++;
+        model2.setCompte(count2);
+        modifyTextView2(count2);
+    }
+
+    private void modifyTextView(int count){
+        TextView t = getView().findViewById(R.id.countText);
+        t.setText(String.valueOf(count));
+    }
+
+    private void modifyTextView2(int count2){
+        TextView t = getView().findViewById(R.id.countText2);
+        t.setText(String.valueOf(count2));
     }
 }
